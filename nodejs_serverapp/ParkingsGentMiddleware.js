3b2afd61ad367ee320d5eb40e8a3ee9463da74e8
Jordy Van Kerkvoorde <44192604+JordyVanKerkvoorde@users.noreply.github.com>
2020-03-28 00:03:23 +0100

    Add sql connection + insertions

--- nodejs_serverapp/ParkingsGentMiddleware.js
+++ nodejs_serverapp/ParkingsGentMiddleware.js
@@ -1,6 +1,8 @@
 // 'https://datatank.stad.gent/4/mobiliteit/bezettingparkingsrealtime.json'
 console.log("applicatie succesvol opgestart: Parkeergarages")
 const fetch = require("node-fetch");
+const sql = require('mssql');
+
 function fetchRequest(url) {
 	return fetch(url)
 		.then(body => body.json()); //retourneert promise object met response
@@ -11,8 +13,10 @@ function getData(){
     .then(result => {
         let parkInit = new ParkingInitializer();
         parkInit.addParkings(result);
-        //afprinten van data later veranderen door .NET API POST
         console.log(parkInit.parkings);
+        parkInit.parkings.forEach((parking) =>{
+          databaseconn(parking);
+        });
     })
     .catch(reject => {
         console.log(reject);
@@ -32,7 +36,9 @@ class ParkingInitializer{
       p.latitude, 
       p.longitude, 
       p.parkingStatus.availableCapacity, 
-      p.parkingStatus.totalCapacity));
+      p.parkingStatus.totalCapacity,
+      p.parkingStatus.lastModifiedDate
+      ));
   }
   get parkings(){
     return this._parkData;
@@ -46,8 +52,37 @@ class Parking{
     this._name = name;
     this._latitude = latitude;
     this._longtitude = longtitude;
-    this._availablecap = availablecap;
-    this._totalcap = totalcap;
+    this._available = availablecap;
+    this._maxcap = totalcap;
+    this._type = "car";
+  }
+}
+
+async function databaseconn(parking){
+  var name = parking._name;
+  var type = parking._type;
+  var latitude = "" + parking._latitude;
+  var longtitude = "" + parking._longtitude;
+  var maxcap = parseInt(parking._maxcap);
+  var availablecap = parseInt(parking._available);
+
+   try{
+     //connection string afgeschermd, database draait op externe server
+       let pool = await sql.connect('Server=xxx.xxx.xxx.xxx,51433;Database=Parkings;User Id=xxxxx;Password=xxxxxxx;');
+         await pool.request().query(`
+          if not exists (select 1 from [Parking] where [Name] = '${name}')
+          begin
+          insert into Parking ([Name], [Type], [Latitude], [Longtitude], [MaxCap]) 
+          values ('${name}', '${type}', '${latitude}', '${longtitude}', ${maxcap})
+          end
+        `);
+        await pool.request().query(`
+        insert into [Entry] ([TimeDay], [Available], [ParkingId]) 
+        values (CURRENT_TIMESTAMP, ${availablecap}, (select Id from Parking where Name = '${name}'))
+        `);
+      pool.close();
+  } catch (err){
+    console.log(err);
   }
 }
 
